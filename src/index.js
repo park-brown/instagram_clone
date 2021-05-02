cb5002ed870795a4a760a99e8f8b228f8fae164f
Park cloud <340688748@qq.com>
2021-05-02 16:50:25 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import store from './store';
+import { Provider } from 'react-redux';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</Provider>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
